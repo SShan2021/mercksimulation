9dd7ef9126d0d2c66ce4856406e2302e8cb6c0f1
S Shan <sophshan@ss-macbook-pro.wireless-1x.unc.edu>
2024-01-25 05:47:16 -0500

    Add generate_beta.R

---
+++ R/generate_beta.R
@@ -0,0 +1,20 @@
+#' Generate a random beta vector
+#'
+#' Generate a random beta vector for input into the grid function.
+#' Specify the number of zeros, and the number of parameters you want.
+#'
+#' @param length_zero How models you want withOUT that variable as a predictor.
+#' @param length_value How models you want WITH that variable as a predictor.
+#'
+#' @return A vector of values for the variable.
+#'
+#' @examples
+#' TXGROUP.X <- generate_beta(length_zero = 3, length_value = 9)
+#'
+#'
+#'
+#' @export
+generate_beta <- function(length_zero, length_value) {
+  beta_value <- runif(1, min = -1, max = 1)
+  beta <- c(rep(0, length_zero), rep(beta_value, length_value))
+}
