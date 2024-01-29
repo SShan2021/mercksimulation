1cd224cd166ea2927e82da46e032d3c6ff5a5952
S Shan <sophshan@ss-macbook-pro.wireless-1x.unc.edu>
2024-01-28 20:37:09 -0500

    fixed save_rds

--- R/save_rds.R
+++ R/save_rds.R
@@ -12,7 +12,7 @@
 #' @return
 #'
 #' @examples
-#' save_rds(simulation_output, location = "/work/users/s/o/sophshan/")
+#' save_rds(simulation_output, location = "/work/users/s/o/sophshan/simulation_sc12_n1000/results_sim/")
 #'
 #'
 #'
@@ -20,9 +20,9 @@
 save_rds <- function(simulation_output, parameter_list, model, n_rep, location){
 
   #extract the n_rep from the parameter_list
-  n_rep <- parameter_list$n_total
+  n <- parameter_list$n_total
 
   #save the files
-  save(simulation_output, file=paste(location, model, "/", "output_", model, "_rep", n_rep, "_n",
-                                      sample_size, ".RData", sep=""))
+  save(simulation_output, file=paste(location, "output_model", model, "_rep", n_rep,
+                                     "_n", n, ".RData", sep=""))
 }
