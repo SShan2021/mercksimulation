5950cf5c5ec703a114e729dfc156ed58c948ebda
S Shan <sophshan@ss-macbook-pro.wireless-1x.unc.edu>
2024-02-13 23:10:55 -0500

    Updated generate beta function to specify minimum and maximum

--- R/generate_beta.R
+++ R/generate_beta.R
@@ -5,16 +5,20 @@
 #'
 #' @param length_zero How models you want withOUT that variable as a predictor.
 #' @param length_value How models you want WITH that variable as a predictor.
+#' @param min The minimum value for generation
+#' @param max The maximum value for generation
 #'
 #' @return A vector of values for the variable.
 #'
 #' @examples
-#' TXGROUP.X <- generate_beta(length_zero = 3, length_value = 9)
+#' TXGROUP.X <- generate_beta(length_zero = 3, length_value = 9,
+#'                            min = -1, max = 1)
 #'
 #'
 #'
 #' @export
-generate_beta <- function(length_zero, length_value) {
-  beta_value <- runif(1, min = -1, max = 1)
+generate_beta <- function(length_zero, length_value,
+                          min, max) {
+  beta_value <- runif(1, min = min, max = max)
   beta <- c(rep(0, length_zero), rep(beta_value, length_value))
 }
